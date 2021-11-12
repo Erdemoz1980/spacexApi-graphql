b8f5bf8148a418c68c52a89f18532385db9dcd49
Erdem Ozdemir <erdemozproductions@gmail.com>
2021-11-12 13:02:01 -0500

    mongodb connected and models created

--- client/server.js
+++ client/server.js
@@ -2,10 +2,13 @@ const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
-dotenv.config({ path: './config/config' });
+dotenv.config({ path: './config/config.env' })
+connectDB();
 
 const app = express();
+app.use(cors());
 const PORT = process.env.PORT;
 
 
